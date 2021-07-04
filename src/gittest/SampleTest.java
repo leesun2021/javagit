cee1c4034bef97b590157f6fdc2d1f362741074b
lee <megapie2021@gmail.com>
2021-07-04 13:59:33 +0900

    sample5 추가

--- src/gittest/SampleTest.java
+++ src/gittest/SampleTest.java
@@ -8,6 +8,7 @@ public class SampleTest {
        System.out.println("hello2");
        System.out.println("hello3");
        System.out.println("hello4");
+       System.out.println("hello5");
 	}
 
 }
